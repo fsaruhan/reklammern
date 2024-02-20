be3d3946dd93b835b1c1f051933434c6920a521a
fsaruhan <afatihsaruhan@gmail.com>
2024-02-20 23:23:00 +0200

    yonlendirmeler ve basit header

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
