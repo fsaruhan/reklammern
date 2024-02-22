615a81dee274a997bbff368cb6daa4758968fc0a
fsaruhan <afatihsaruhan@gmail.com>
2024-02-22 20:42:43 +0200

    test api for user

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
-import express from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.mongo)
   .then(() => {
     console.log("Connected to DB....");
   })
@@ -16,3 +17,11 @@ const app = express();
 app.listen(3000, () => {
   console.log("Sunucu çalışıyor..");
 });
+
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Hello world!",
+  });
+});
+
+app.use("/api/user", userRouter);
