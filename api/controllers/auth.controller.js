b858b13c0fc5a2e2773f73ceab66b3d2c9baf3b2
fsaruhan <afatihsaruhan@gmail.com>
2024-02-29 16:59:32 +0200

    token.js klaösürü kurumu ve auth.controller düzenlenmesi

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import Token from "../models/token.js";
+import mongoose from "mongoose";
+import crypto from "crypto";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +11,12 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "Kayıt başarılı!" });
+    const token = new Token({
+      userId: new mongoose.Types.ObjectId(),
+      token: crypto.randomBytes(16).toString("hex"),
+    }); // YORUM İÇİN BEKLE....
+    await token.save();
+    console.log(token);
   } catch (error) {
     next(error);
   }
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    mailIsVerified: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
   },
   { timestamps: true }
 );
